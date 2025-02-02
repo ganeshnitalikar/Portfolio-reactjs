ae2ca9574aa7625868b202b6bf97fac8589e9e57
Ganesh <ganeshnitalikar@gmail.com>
2025-02-02 22:19:13 +0530

    New project added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,49 +1,67 @@
-import project1 from '../../public/ReelRaveMockUp.png'
-import project2 from '../../public/ChatterMockUp.png'
-import project4 from '../../public/project4.png'
+import project1 from "../../public/ReelRaveMockUp.png";
+import project2 from "../../public/ChatterMockUp.png";
+import project4 from "../../public/project4.png";
+import talkify from "../../public/talkify.png";
 
-export const HERO_CONTENT = `I’m a Computer Engineering graduate from Sinhgad Institute of Technology and Science, Narhe, Pune, where I maintained a strong academic record with a CGPA of 8.9. My time at college not only provided me with a solid technical foundation but also shaped my problem-solving skills and ability to work collaboratively. I believe in balancing academics with real-world experience, and that mindset has driven my passion for continuous learning and growth in the field of software development.`;
+export const HERO_CONTENT = `I’m a Full Stack Developer specializing in the MERN stack and Java, with a degree in Computer Engineering from Sinhgad Institute of Technology and Science, Pune. With a strong academic record and hands-on experience in building full-stack web applications, I am passionate about creating efficient and scalable solutions that cater to real-world needs. My journey has been defined by continuous learning, practical application, and a drive to make impactful contributions in the field of software development.`;
 
-export const ABOUT_PARA1 = `Hi, I’m Ganesh Nitalikar, a passionate software developer with a knack for solving complex problems through code. As a Computer Engineering graduate with hands-on experience in technologies like Java, React, and Flutter, I enjoy building innovative solutions that are both functional and visually appealing. My journey in software development has been shaped by projects that span from mobile apps to full-stack web applications, each adding a unique perspective to my skillset.`;
-export const ABOUT_PARA2 = `My technical skills include proficiency in frontend technologies like React and JavaScript, as well as mobile app development with Flutter. I’m also well-versed in Java for backend development and use Firebase for real-time applications and database management. In addition, I actively use Git and GitHub for version control, collaborating with teams on projects, and ensuring smooth development workflows. My diverse skill set allows me to approach challenges from multiple angles and find the best solutions`
-export const ABOUT_PARA3 = `I’m always eager to contribute to projects and take on new challenges. Whether it’s through internships, personal projects, or collaborations, I actively seek opportunities to apply my knowledge and learn from others. I believe in the power of hands-on experience and strive to stay updated with the latest technologies and industry trends. I’m committed to growing as a developer, and I’m excited to take on roles where I can continue to contribute and make a meaningful impact.`
+export const ABOUT_PARA1 = `Hi, I’m Ganesh Nitalikar, a Full Stack Developer skilled in the MERN stack (MongoDB, Express.js, React, Node.js) and Java. My academic background in Computer Engineering, coupled with practical experience, has enabled me to develop robust web applications and effective backend solutions.`;
+export const ABOUT_PARA2 = `My technical skills encompass full-stack development with the MERN stack, allowing me to create dynamic web applications from scratch. Additionally, my proficiency in Java equips me with the ability to build efficient backend systems. I utilize Git and GitHub for version control, ensuring collaborative and seamless development processes.`;
+export const ABOUT_PARA3 = `Driven by a passion for technology and innovation, I continually seek out opportunities to expand my knowledge and tackle challenging projects. Whether it's through personal projects, internships, or collaborative work, I am committed to leveraging my skills to deliver impactful solutions and grow as a developer.`;
 
 export const PROJECTS = [
   {
+    title: "Talkify",
+    image: talkify,
+    link: "https://github.com/ganeshnitalikar/talkify",
+    description:
+      "Talkify is a modern real-time chat application designed for seamless and instant communication. Built with React and Zustand for a responsive frontend and powered by Node.js, Express, and MongoDB on the backend, it ensures efficiency and scalability. Utilizing Socket.io, Talkify enables smooth bidirectional messaging, while JWT authentication with cookies keeps user sessions secure.",
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Socket.io",
+      "Zustand",
+    ],
+  },
+
+  {
+    title: "CodeSphere",
+    image: "https://placehold.co/600x300/black/white",
+    link: "https://github.com/ganeshnitalikar/CodeSphere",
+    description:
+      "CodeSphere is a feature-rich online IDE built using the MERN stack. It offers real-time coding, user authentication, and a seamless development environment directly in the browser, enabling developers to code efficiently from anywhere.",
+    technologies: ["MongoDB", "Express.js", "React", "Node.js"],
+  },
+  {
     title: "ReelRave",
     image: project1,
-    link : "https://github.com/ganeshnitalikar/ChatApp-in-Flutter-using-Firebase",
+    link: "https://github.com/ganeshnitalikar/ReelRave",
     description:
-      "ReelRave is a short video-sharing platform inspired by TikTok. It allows users to create and upload videos with features such as liking, commenting, and sharing. The app ensures smooth performance through video compression and supports user authentication for a personalized experience. ReelRave is designed to provide an engaging and interactive platform for creators and viewers alike.",
+      "ReelRave is a short video-sharing platform inspired by TikTok. Built using Flutter and Firebase, it offers features like video uploading, liking, commenting, and user authentication for a personalized experience. ReelRave is designed for an engaging and interactive platform for creators and viewers.",
     technologies: ["Flutter", "Firebase"],
   },
   {
     title: "Chatter",
     image: project2,
-    link: "https://github.com/ganeshnitalikar/ReelRave",
+    link: "https://github.com/ganeshnitalikar/ChatApp-in-Flutter-using-Firebase",
     description:
-      "Chatter is a modern mobile chat application designed to connect users seamlessly. Built with Flutter and Firebase, the app supports features like user authentication, real-time messaging, and user search functionality. It provides a smooth and intuitive user experience, enabling people to stay connected with friends and family in a secure and reliable manner.",
+      "Chatter is a real-time chat application built with Flutter and Firebase. It supports user authentication, real-time messaging, and a user-friendly interface, making it easy for users to stay connected securely and efficiently.",
     technologies: ["Flutter", "Firebase"],
   },
-  {
-    title: "CodeSphere",
-    image: "https://placehold.co/600x400/orange/white",
-    link: "",
-    description:
-      "CodeSphere is an online IDE built using the MERN stack, providing users with an efficient environment for coding and testing. The platform includes features such as user authentication, secure API integration, and live output display on the right side of the editor. With a clean and user-friendly interface, CodeSphere empowers developers to write, debug, and execute code directly in the browser.",
-    technologies: ["MongoDB", "Express.js", "React", "Node.js"],
-  },
+
   {
     title: "Portfolio",
     image: project4,
-    link: "https://ganeshnitalikar.vercel.app/",  
+    link: "https://ganeshnitalikar.vercel.app/",
     description:
-      "This Portfolio App is a sleek and responsive personal website built using React, Tailwind CSS, and JavaScript. It showcases the developer's skills, projects, and professional experience in a visually appealing layout. Designed with modern UI principles, the app provides an excellent way to present oneself to potential employers and collaborators.",
+      "This Portfolio App is a sleek and responsive personal website built using React, Tailwind CSS, and JavaScript. It showcases the developer's skills, projects, and professional experience in a visually appealing layout, offering a comprehensive overview to potential employers and collaborators.",
     technologies: ["React", "Tailwind CSS", "JavaScript"],
   },
 ];
 
 export const CONTACT = {
-  address: "Pune,India",
+  address: "Pune, India",
   email: "ganeshnitalikar@gmail.com",
 };
